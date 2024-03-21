4a261ec7410e7961b619c8011682c901a98648f2
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-03-22 00:55:21 +0900

    Fix: ContentHeader pathname 작업

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import { ToastContainer, ToastContainerProps } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ContentHeader from "./components/ContentHeader";
+import { accessToken } from "./utils/common";
+import { usePathname } from "next/navigation";
 
 interface ProvidersProps extends ToastContainerProps {
   children: React.ReactNode;
@@ -11,11 +14,15 @@ interface ProvidersProps extends ToastContainerProps {
 
 function Providers({ children, ...toastProps }: ProvidersProps) {
   const [client] = React.useState(new QueryClient());
-
+  const pathname = usePathname();
   return (
     <QueryClientProvider client={client}>
       <ReactQueryStreamedHydration>
         <ToastContainer {...toastProps} />
+        {pathname === "/" || pathname === "/auth/register" ? null : (
+          <ContentHeader />
+        )}
+
         {children}
       </ReactQueryStreamedHydration>
     </QueryClientProvider>

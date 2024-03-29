1125ae7ce9c920a772fc517dd093bde977fed090
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-03-30 00:16:32 +0900

    Fix: 회원가입 form 수정 및 회원가입 API 로직 수정

--- src/app/(route)/auth/register/page.tsx
+++ src/app/(route)/auth/register/page.tsx
@@ -3,86 +3,34 @@
 import { Box, Button, CircularProgress, Grid, TextField } from "@mui/material";
 import React, { useState } from "react";
 
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import axios from "axios";
+import AuthApiClient from "@/clients/AuthApiClient";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { API_BASE_URL } from "@/app/utils/common";
-import { pushNotification } from "@/app/utils/notification";
 
 export interface Register {
   username: string;
   password: string;
   name: string;
   mobileNumber: string;
-  birthday: {
-    $d: string;
-  };
+  birthday: string;
   email: string;
-}
-
-function convertToBirthdate(dateString: string): string {
-  const date = new Date(dateString);
-  const year = date.getUTCFullYear().toString().slice(2);
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
-  const day = date.getUTCDate().toString().padStart(2, "0");
-  return `${year}${month}${day}`;
 }
 
 const index = () => {
   const router = useRouter();
-  const [register, setRegister] = useState<Register>({
-    username: "",
-    password: "",
-    name: "",
-    mobileNumber: "",
-    birthday: {
-      $d: "",
-    },
-    email: "",
-  });
+  const [register, setRegister] = useState<Register>();
 
-  // Hook
-  const onChange = (field: string, value: string | any) => {
+  const onChange = (field: keyof Register, value: string) => {
     setRegister((prevState) => ({
-      ...prevState,
+      ...prevState!,
       [field]: value,
     }));
   };
-
-  const fetchRegister = async (params: Register) => {
-    const { username, password, mobileNumber, name, birthday, email } = params;
-    await axios
-      .post(
-        `${API_BASE_URL}/auth/signup`,
-        {
-          username: username,
-          password: password,
-          mobileNumber: mobileNumber,
-          birthday: convertToBirthdate(birthday.$d),
-          name: name,
-          email: email,
-        },
-        {
-          headers: { "Content-Type": "application/json" },
-        }
-      )
-      .then((res) => res?.data.message);
-  };
-
-  const { mutate, data, isLoading } = useMutation(fetchRegister, {
-    onSuccess: () => {
-      pushNotification("회원가입이 되었습니다.", "success");
-      router.push("/");
-    },
-    onError: (error) => {
-      pushNotification("에러가 발생하였습니다.", "error");
-    },
-    onSettled: () => {},
+  const { mutate: siginUpMutation, isLoading } = useMutation({
+    mutationKey: [""],
+    mutationFn: () => AuthApiClient.getInstance().userRegister(register!),
   });
+
   if (isLoading) return <CircularProgress color="inherit" />;
 
   return (
@@ -155,17 +103,7 @@ const index = () => {
           <Grid item xs={5}>
             <div>Birthday</div>
           </Grid>
-          <Grid item xs={7}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["DatePicker"]}>
-                <DatePicker
-                  label="Controlled picker"
-                  value={register?.birthday}
-                  onChange={(e) => onChange("birthday", e)}
-                />
-              </DemoContainer>
-            </LocalizationProvider>
-          </Grid>
+          <Grid item xs={7}></Grid>
           {/* <Grid item xs={5}>
             <div>Gender</div>
           </Grid>
@@ -204,7 +142,7 @@ const index = () => {
             variant="contained"
             color="primary"
             onClick={() => {
-              mutate(register);
+              siginUpMutation();
             }}
           >
             저장
